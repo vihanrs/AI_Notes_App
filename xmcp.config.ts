073ff2272c4c2cdbf697f548116fc76efe96c021
vihanrs <vihanvrs@gmail.com>
2026-01-27 00:50:46 +0530

    feat: set `prompts` and `resources` paths to `false` in `xmcp.config.ts`

--- xmcp.config.ts
+++ xmcp.config.ts
@@ -1,18 +1,18 @@
 import { type XmcpConfig } from "xmcp";
 
 const config: XmcpConfig = {
-  http: true,
-  experimental: {
-    adapter: "nextjs",
-  },
-  paths: {
-    tools: "tools",
-    prompts: "prompts",
-    resources: "resources",
-  },
-  typescript: {
-    skipTypeCheck: true,
-  },
+    http: true,
+    experimental: {
+        adapter: "nextjs",
+    },
+    paths: {
+        tools: "tools",
+        prompts: false,
+        resources: false,
+    },
+    typescript: {
+        skipTypeCheck: true,
+    },
 };
 
 export default config;
